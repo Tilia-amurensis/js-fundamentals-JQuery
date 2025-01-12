01d04c7d3742eaba32b10084316a485b3e84e0ce
Alina-Kolomiitseva <53407424+Tilia-amurensis@users.noreply.github.com>
2025-01-12 18:38:15 +0200

    Added all solved tasks

---
+++ task4.js
@@ -0,0 +1,12 @@
+// На HTML-сторінці є 6 чекбоксів.
+// Напишіть скріпт, який після того,
+// як користувач позначив будь-які 3 чекбокси,
+// всі чекбокси робить неактивними.
+
+// Завдання виділене вставкою Task4
+$("input['type=checkbox']").on("change", function () {
+  const checked = $("input['type=checkbox']").is(":checked").length;
+  if (checked <= 3) {
+    $("input['type=checkbox']").prop("disabled", true);
+  }
+});
